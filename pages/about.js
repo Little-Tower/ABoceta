da4af8d9781e244448689ae8d8720e0abe74a12f
Little-Tower <higuazu@gmail.com>
2021-02-23 21:15:48 +0100

    Foto de about

--- pages/about.js
+++ pages/about.js
@@ -9,24 +9,21 @@ const About = () => {
             </Head>
             <div className={styles.cuerpo}>
                 <div className={styles.articulo}>
-                    <p class={styles.titulos}>Esto somos nosotros:</p>
+                    <h1 className={styles.titulos}>Esto somos nosotros:</h1>
                     <div className={styles.descripcion}>
 
                         <p>Contamos con un amplio equipo de profesionales que garantizan
                         la correcta gestión de los asuntos con un trato directo día a día
-                entre los partidos judiciales de Sevilla y provincia.</p>
+                                entre los partidos judiciales de Sevilla y provincia.</p>
 
                         <p>Nuestro despacho está con una constante renovanción, incorporando
                         las últimas tecnología para hacer una gestión más ágil y eficaz de cada caso.
-                        </p>
+                                 </p>
 
                         <p>Nuestro despacho trabaja con el software líder en gestión legal Aranzadi.
                         Los mejores trabajan con los mejores y, Aranzadi junto a nuestro despacho
                         avalan y aseguran la rápida administración de cada caso.
-                        </p>
-                    </div>
-                    <div className={styles.foto}>
-                        <img className={styles.foto2} src="img/juzgados.jpg" alt="Foto juzgados" />
+                                </p>
                     </div>
                 </div>
             </div>
